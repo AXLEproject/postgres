3cd65741cfe8d8618d0428d938e263627734b6be
Adria Armejach <miconof@gmail.com>
2014-09-26 15:39:01 +0200

    back to 8 byte blocks

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname "$0")
 BASEDIR=$(cd "$BASEDIR"; pwd)
 
 MAKETEXT='
-CFLAGS+=-mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a -mno-avx -I${ZSIMPATH}/misc/hooks\n
+CFLAGS+=-mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a -mno-avx -march=core2 -I${ZSIMPATH}/misc/hooks\n
 '
 
 DEST=$BASEDIR/build/bin/postgres
@@ -13,7 +13,7 @@ if [ ! -r $DEST ] || [ $(find src -newer $DEST | wc -l) -gt 0 ]; then
         # compile
         mkdir -p $BASEDIR/build
         cd $BASEDIR/build
-        CFLAGS="-fno-omit-frame-pointer -rdynamic" $BASEDIR/configure --prefix=$BASEDIR/build --enable-debug --with-blocksize=32 --with-wal-blocksize=64
+        CFLAGS="-fno-omit-frame-pointer -rdynamic -O2" $BASEDIR/configure --prefix=$BASEDIR/build --enable-debug --with-blocksize=8 --with-wal-blocksize=8
         make clean
         echo -e $MAKETEXT >> $BASEDIR/build/src/Makefile.global
         make -j$(grep -c ^processor /proc/cpuinfo)
