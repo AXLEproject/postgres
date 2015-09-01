75c2e3ed66f1d7e8f2441fa257d01bdb93c7086c
Adria Armejach <miconof@gmail.com>
2015-09-01 11:50:45 +0200

    New storage backend for memory mapped files
    * Added new storage backend folder (mmfile). Currently is a copy of the
    original file folder.
    * Modified Makefile to compile new folder if the STORAGE env variable is
    set to "MMFILE"

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ ! -r $DEST ] || [ $(find src -newer $DEST | wc -l) -gt 0 ]; then
         cd $BASEDIR/build
         CFLAGS="-fno-omit-frame-pointer -rdynamic -O2" $BASEDIR/configure --prefix=$BASEDIR/build --enable-debug --with-blocksize=8 --with-wal-blocksize=8
         make clean
-        make -j$(grep -c ^processor /proc/cpuinfo)
+        STORAGE="MMFILE" make -j$(grep -c ^processor /proc/cpuinfo)
         make install
         echo "done. Cleaning intermediate make state"
         make clean
