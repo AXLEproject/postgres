1921e532ac4c161c713a429aa0c555c2db17aa38
Adria Armejach <miconof@gmail.com>
2015-11-05 14:37:30 +0100

    moving build dir outside

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 BASEDIR=$(dirname "$0")
 BASEDIR=$(cd "$BASEDIR"; pwd)
 
-DEST=$BASEDIR/build/pg_pmfs_experimental/bin/postgres
+DEST=$BASEDIR/../pg-build/pg_pmfs_experimental/bin/postgres
 
 if [ ! -r $DEST ] || [ $(find src -newer $DEST | wc -l) -gt 0 ]; then
         # compile
-        mkdir -p $BASEDIR/build/pg_pmfs_experimental
-        cd $BASEDIR/build/pg_pmfs_experimental
-        CFLAGS="-fno-omit-frame-pointer -rdynamic -O2" $BASEDIR/configure --prefix=$BASEDIR/build/pg_pmfs_experimental --with-blocksize=8 --with-wal-blocksize=8
+        mkdir -p $BASEDIR/../pg-build/pg_pmfs_experimental
+        cd $BASEDIR/../pg-build/pg_pmfs_experimental
+        CFLAGS="-fno-omit-frame-pointer -rdynamic -O2 -g -ggdb" $BASEDIR/configure --prefix=$BASEDIR/../pg-build/pg_pmfs_experimental --with-blocksize=8 --with-wal-blocksize=8
         #make clean
         make -j$(grep -c ^processor /proc/cpuinfo)
         make install
-        echo "done"
         #make clean
+        echo "done"
 else
         echo ""
         echo "Already up to date. Skip compilation."
@@ -23,7 +23,7 @@ fi
 echo "Creating symlinks"
 rm -rf $BASEDIR/build/bin/
 mkdir -p $BASEDIR/build/bin/
-ln -s $BASEDIR/build/pg_pmfs_experimental/bin/* $BASEDIR/build/bin
+ln -s $BASEDIR/../pg-build/pg_pmfs_experimental/bin/* $BASEDIR/build/bin
 
 echo "Add 'PGPATH=$BASEDIR' to your environment."
 
