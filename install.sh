80e8a3978e9a38a1f4e76e172cc5f9c37f7abc0e
Adria Armejach <miconof@gmail.com>
2015-11-05 14:57:53 +0100

    Build dir outside pg folder

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 BASEDIR=$(dirname "$0")
 BASEDIR=$(cd "$BASEDIR"; pwd)
 
-DEST=$BASEDIR/build/pg_pmfs/bin/postgres
+DEST=$BASEDIR/../pg-build/pg_pmfs/bin/postgres
 
 if [ ! -r $DEST ] || [ $(find src -newer $DEST | wc -l) -gt 0 ]; then
         # compile
-        mkdir -p $BASEDIR/build/pg_pmfs
-        cd $BASEDIR/build/pg_pmfs
-        CFLAGS="-fno-omit-frame-pointer -rdynamic -O2" $BASEDIR/configure --prefix=$BASEDIR/build/pg_pmfs --with-blocksize=8 --with-wal-blocksize=8
+        mkdir -p $BASEDIR/../pg-build/pg_pmfs
+        cd $BASEDIR/../pg-build/pg_pmfs
+        CFLAGS="-fno-omit-frame-pointer -rdynamic -O2" $BASEDIR/configure --prefix=$BASEDIR/../pg-build/pg_pmfs --with-blocksize=8 --with-wal-blocksize=8
         #make clean
         make -j$(grep -c ^processor /proc/cpuinfo)
         make install
@@ -23,7 +23,7 @@ fi
 echo "Creating symlinks"
 rm -rf $BASEDIR/build/bin/
 mkdir -p $BASEDIR/build/bin/
-ln -s $BASEDIR/build/pg_pmfs/bin/* $BASEDIR/build/bin
+ln -s $BASEDIR/../pg-build/pg_pmfs/bin/* $BASEDIR/build/bin
 
 echo "Add 'PGPATH=$BASEDIR' to your environment."
 
