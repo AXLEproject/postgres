db3c6f9d0dcad3ee95fddd07bdbbd16e5d610830
Adria Armejach <miconof@gmail.com>
2015-11-05 15:13:19 +0100

    disable debug

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [ ! -r $DEST ] || [ $(find src -newer $DEST | wc -l) -gt 0 ]; then
         # compile
         mkdir -p $BASEDIR/../pg-build/pg_pmfs_experimental
         cd $BASEDIR/../pg-build/pg_pmfs_experimental
-        CFLAGS="-fno-omit-frame-pointer -rdynamic -O2 -g -ggdb" $BASEDIR/configure --prefix=$BASEDIR/../pg-build/pg_pmfs_experimental --with-blocksize=8 --with-wal-blocksize=8
+        CFLAGS="-fno-omit-frame-pointer -rdynamic -O2" $BASEDIR/configure --prefix=$BASEDIR/../pg-build/pg_pmfs_experimental --with-blocksize=8 --with-wal-blocksize=8
         #make clean
         make -j$(grep -c ^processor /proc/cpuinfo)
         make install
